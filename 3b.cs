ec93cbcb5c0db73b1d0a5b2539e2b067110faf51
Oliwier601 <113879766+Oliwier601@users.noreply.github.com>
2022-10-23 21:43:14 +0200

    Create 3b.cs

---
+++ 3b.cs
@@ -0,0 +1,44 @@
+//Zad 1
+/*for (int i = 1; i <= 30; i++)
+{
+    Console.WriteLine(i);
+    
+}*/
+
+//Zad 2
+/*for (int i = 1; i <=9; i+=2)
+{
+    Console.WriteLine(i*i);
+}*/
+
+//Zad 3
+/*for (int i = 1000; i <= 9999; i++)
+{
+    if(i % 379 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}*/
+
+//Zad 4  
+/*for (int i = 100; i <= 999; i++)
+{
+    if (i % 5 == 0 || i % 6 == 0 || i % 11 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}*/
+
+//Zad 5
+/*Console.WriteLine("Ile liczb");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+int c;
+for (int i = 1; i <= a; i++)
+{
+    Console.WriteLine("Liczba nr " + i);
+    c  = int.Parse(Console.ReadLine());
+    b += c;
+}
+Console.WriteLine("Suma");
+Console.WriteLine(b);*/
